a562d2fff54ebe43e3e8e664c94a512564bda5bc
Kim Wong <think.kimba@gmail.com>
2023-08-23 12:10:35 +0800

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  # get 'listings/index'
-  # get 'listings/:id/show', to: 'listings#show'
-  # get 'listings/new'
-  # get 'listings/create'
-  # get 'listings/edit'
-  # get 'listings/update'
-  # get 'listings/destroy'
-
-  resources :listings
-
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :listings
 end
