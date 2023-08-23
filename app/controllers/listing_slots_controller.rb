cd1728da06daca0fbe058755b5252376fcb2a19b
Millie Pratley <millie.pratley@icloud.com>
2023-08-23 18:59:07 +0800

    Functionality added to create new listing slots, and new bookings

--- app/controllers/listing_slots_controller.rb
+++ app/controllers/listing_slots_controller.rb
@@ -1,4 +1,27 @@
 class ListingSlotsController < ApplicationController
   def index
   end
+
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @listing_slot = ListingSlot.new
+  end
+
+  def create
+    @listing_slot = ListingSlot.new(listing_slot_params)
+    @listing = Listing.find(params[:listing_id])
+    @listing_slot.listing = @listing
+
+    if @listing_slot.save
+      redirect_to @listing, notice: "Slot was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def listing_slot_params
+    params.require(:listing_slot).permit(:start_time, :end_time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :listings
+  resources :listings do
+    resources :listing_slots, only: [:new, :create] do
+      resources :bookings, only: [:new, :create]
+    end
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
 end
