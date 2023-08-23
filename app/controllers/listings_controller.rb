4d6af23731f94e95b3b5aa05b6d1541ec0271b79
Kim Wong <think.kimba@gmail.com>
2023-08-23 12:07:15 +0800

    add show for listing and updated routes.rb

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,7 +3,7 @@ class ListingsController < ApplicationController
   end
 
   def show
-
+    @listing = Listing.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'listings/index'
-  get 'listings/show'
-  get 'listings/new'
-  get 'listings/create'
-  get 'listings/edit'
-  get 'listings/update'
-  get 'listings/destroy'
+  # get 'listings/index'
+  # get 'listings/:id/show', to: 'listings#show'
+  # get 'listings/new'
+  # get 'listings/create'
+  # get 'listings/edit'
+  # get 'listings/update'
+  # get 'listings/destroy'
+
+  resources :listings
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
