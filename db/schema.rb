5924c042fe1be243d88713deffdf36085c5aa8d6
Millie Pratley <millie.pratley@icloud.com>
2023-08-24 11:54:37 +0800

    Listing slots are bookable and only show if they're available to book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_062805) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_032505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_062805) do
     t.date "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "booked"
     t.index ["listing_id"], name: "index_listing_slots_on_listing_id"
   end
 
