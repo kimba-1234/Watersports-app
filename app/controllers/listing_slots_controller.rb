85971357b1cb1904d84ee79a421977470a5ef55b
Kim Wong <think.kimba@gmail.com>
2023-08-25 15:18:56 +0800

    show

--- app/controllers/listing_slots_controller.rb
+++ app/controllers/listing_slots_controller.rb
@@ -1,5 +1,5 @@
 class ListingSlotsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :index, :show]
+#before_action :authenticate_user!, only: [:new, :create, :index, :show]
 
   def index
     @listing_slots = ListingSlot.all
